4b64cb8e78dbfbbf9d78d60084e114a6bcb84fa8
cyt199709 <464124631@qq.com>
2021-11-08 14:39:05 +0800

    补充CommonUtils类

--- CommonUtils.cpp
+++ CommonUtils.cpp
@@ -1,1 +1,44 @@
 #include "CommonUtils.h"
+#include <QFile>
+#include <QApplication>
+#include <QSettings>
+
+CommonUtils::CommonUtils()
+{
+}
+
+void CommonUtils::loadStyleSheet(QWidget* widget, const QString& sheetName)
+{
+	QFile file(":/Resources/QSS/" + sheetName + ".css");
+	file.open(QIODevice::ReadOnly);
+	if (file.isOpen())
+	{
+		widget->setStyleSheet("");
+		QString qssStyleSheet = QLatin1String(file.readAll());
+		widget->setStyleSheet(qssStyleSheet);
+	}
+	file.close();
+}
+
+void CommonUtils::setDefaultSkinColor(const QColor& color)
+{
+	const QString& path = QApplication::applicationDirPath() + "/" + QString("tradeprintinfo.ini");
+	QSettings settings(path, QSettings::IniFormat);
+	settings.setValue("DefaultSkin/red", color.red());
+	settings.setValue("DefaultSkin/green", color.green());
+	settings.setValue("DefaultSkin/blue", color.blue());
+}
+
+QColor CommonUtils::getDefaultSkinColor()
+{
+	const QString& path = QApplication::applicationDirPath() + "/" + QString("tradeprintinfo.ini");
+	if (!QFile::exists(path))
+		setDefaultSkinColor(QColor(240, 225, 229));
+
+	QSettings settings(path, QSettings::IniFormat);
+	QColor color;
+	color.setRed(settings.value("Default/red").toInt());
+	color.setGreen(settings.value("Default/green").toInt());
+	color.setBlue(settings.value("Default/blue").toInt());
+	return color;
+}
